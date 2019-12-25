7f50593ac588d637145cbabbeb9b97248b72d9ef
RiSer <sergiy19981@gmail.com>
2019-12-25 11:09:24 +0100

    update user administration

--- src/ekka/core.clj
+++ src/ekka/core.clj
@@ -1,6 +1,74 @@
 (ns ekka.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def user-database [])
+
+(var user-database)
+
+(defn printuj []
+  (println "chuj tam"))
+
+
+(defrecord User [credential permission email name last_name])
+(defrecord Credential [login password])
+(defrecord Permission [id permission])
+
+(do
+  (def user-database [])
+  (def user-database (conj user-database
+                           (User. (Credential. "admin" "admin")
+                                  (Permission. 1 "admin")
+                                  "admin@admin.pl"
+                                  "Admin"
+                                  "Administrowicz")
+                           (User. (Credential. "Tomasz" "Pilot")
+                                  (Permission. 2 "user")
+                                  "user@user.pl"
+                                  "Tomasz"
+                                  "Pilot"))))
+
+(:email (User. (Credential. "admin" "admin")
+               (Permission. 1 "admin")
+               "admin@admin.pl"
+               "Admin"
+               "Administrowicz"))
+
+(defn login [])
+(defn register [])
+(defn test-user [credential]
+  (letfn [(comprt [f c1 c2] (= (f c1) (f c2)))]
+    (first (filter #((and
+                      (comprt credential %1)
+                      (comprt credential %2)))))))
+
+(defmacro create-field-and
+  ([] false)
+  ([key-typed]
+   (fn [object-to-compare]
+     `(create-field-and key-typed object-to-compare)))
+  ([key-typed1 key-typed2]
+   (let [a (for [i (keys key-typed1)]
+            `(= (~i ~key-typed1) (~i ~key-typed2)))]
+     `(and ~@a))))
+
+((create-field-and (User. (Credential. "admin" "admin")
+                          (Permission. 1 "admin")
+                          "admin@admin.pl"
+                          "Admin"
+                          "Administrowicz")) (User. (Credential. "admin" "admin")
+                         (Permission. 1 "admin")
+                         "admin@admin.pl"
+                         "Admin"
+                         "Administrowicz"))
+
+(create-field-and (User. (Credential. "admin" "admin")
+                          (Permission. 1 "admin")
+                          "admin@admin.pl"
+                          "Admin"
+                          "Administrowicz")
+                  (User. (Credential. "admin" "admin")
+                          (Permission. 1 "admin")
+                          "admin@admin.pl"
+                          "Admin"
+                          "Administrowicz"))
+

1ad9e5324f8e178fd33c6105660858c3810d8a99
RiSer <sergiy19981@gmail.com>
2020-02-12 03:21:53 +0100

    create key-value table clojure interface

--- src/ekka/lib/datatool/key_value_tool.clj
+++ src/ekka/lib/datatool/key_value_tool.clj
@@ -2,8 +2,7 @@
   (:require
    [clojure.string :as string]
    ;; [ekka.lib.datatool.sql-tool :as toolbox :refer [where-string]]
-   [ekka.lib.datatool.sql-tool :as toolbox :include-macros true
-    :refer [where-string]]))
+   [ekka.lib.datatool.sql-tool :as toolbox :include-macros true :refer :all]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -32,37 +31,58 @@
                   (table-column "value" (format "%s DEFAULT NULL" value-type))
                   "PRIMARY KEY (`id`)"]]
      (binding [toolbox/*where-border* true]
-       (format "CREATE TABLE IF NOT EXIST `%s` %s" table-name 
+       (format "CREATE TABLE IF NOT EXIST `%s` %s" (name table-name) 
                (toolbox/into-border (string/join ", " columns)))))))
 
 (defn create-table [table-name & {:keys [engine key-type value-type]
                                   :or {key-type *key-column-default-type*
-                                       value-type *value-column-default-type*} }]
+                                       value-type *value-column-default-type*
+                                       engine "InnoDB"}}]
   (format "%s %s" (create-pair-table table-name key-type value-type)
           (format "ENGINE=%s DEFAULT CHARSET=utf8;"
                   (if (some #(= % engine) *available-mariadb-engine-list*)
                     engine "InnoDB"))))
 
-;;; to test 
-(create-table "suka" :key-type "VARCHAR(20)" :engine "MEMORY")
-(create-table "suka")
+
+(defn drop-table [table-name]
+  (format "DROP TABLE IF EXISTS `%s`;" (name table-name)))
+
+;; (create-table :suka :key-type "VARCHAR(20)" :engine "MEMORY")
+;; (create-table :suka :key-type "VARCHAR(20)")
+;; (create-table :suka :engine "MEMORY")
+;; (create-table :suka)
+;; (drop-table :suka)
 
 
-(defmacro get-key [key]
-  `(toolbox/select :suka
-                   :where `(= :key ~key)))
+(defrecord TableAccessors [getter setter destroyer creater cleaner keys values])
 
-(get-key "suak")
+(defn get-table [table-name]
+  (println (create-table table-name :key-type "varchar(20)" :engine "memory"))
+  (apply ->TableAccessors (map #(% table-name) [#'create-getter #'create-setter #'create-destroyer #'create-creater #'create-cleaner #'create-keys #'create-values])))
 
+(defn create-getter [table-name]
+  (fn [key] (select table-name :where (= :key key))))
 
+(defn create-setter [table-name]
+  (fn [key value] (vector (select table-name :where (= :key key))
+                          (update table-name :set {:value value} :where (= :key key))
+                          (insert table-name :values [{:key key :value value}]))))
 
+(defn create-destroyer [table-name]
+  (fn [] (drop-table table-name)))
 
-(defmacro suka [k]
-  `(toolbox/select :suka
-                   :where (= :key ~k)))
+(defn create-creater [table-name &{:keys [engine key-type value-type] :as all}]
+  (fn [] (apply create-table table-name (mapcat identity all))))
 
-(suka "123")
+(defn create-keys [table-name]
+  (fn [] (select table-name :column [:key])))
 
+(defn create-values [table-name]
+  (fn [] (select table-name :column [:values])))
+
+(defn create-cleaner [table-name &{:keys [engine key-type value-type] :as all}]
+  (fn [] (vec (create-destroyer)
+              (apply create-table table-name (mapcat identity all)))))
 
 
 
